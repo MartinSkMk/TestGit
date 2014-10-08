645a116dcf12b5c6d35385dceaf12080a490a3b2
Martin Markovski <mrtn_markovski@yahoo.com>
2014-10-08 19:23:31 +0200

    Changed index.html

--- www/js/app.js
+++ www/js/app.js
@@ -9,6 +9,7 @@ function AppCtrl($scope, FreshlyPressed, $log) {
     $scope.refresh = function() {        
         FreshlyPressed.getBlogs($scope);
         console.log($scope);
+        console.log("this then");
         console.log("end");
     }
 
